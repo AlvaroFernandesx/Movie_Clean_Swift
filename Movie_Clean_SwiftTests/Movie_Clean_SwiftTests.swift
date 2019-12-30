30091fa679b9bf1a0e0c34d2d52532bb8c6994db
Álvaro Fernandes <46978323+alvarozadek@users.noreply.github.com>
2019-12-29 23:10:55 -0200

    Initial Commit

---
+++ Movie_Clean_SwiftTests/Movie_Clean_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  Movie_Clean_SwiftTests.swift
+//  Movie_Clean_SwiftTests
+//
+//  Created by Álvaro Fernandes on 29/12/19.
+//  Copyright © 2019 Álvaro Fernandes. All rights reserved.
+//
+
+import XCTest
+@testable import Movie_Clean_Swift
+
+class Movie_Clean_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
